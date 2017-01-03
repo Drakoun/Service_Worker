972087c53562891704015b259cff12ed1d6b10b1
Drakoun <davidboccara333@yahoo.fr>
2017-01-03 14:27:13 +0100

    init

--- serviceworker.js
+++ serviceworker.js
@@ -1,14 +1,10 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = ['/worker.js','/main.js'];
-
-
 this.addEventListener('install', function(event) {
 	// Perform install steps
 		event.waitUntil(
-			caches.open(CACHE_NAME)
+			caches.open('my-site-cache-v1')
 				.then(function(cache) {
 					console.log('Opened cache');
-					return cache.addAll(urlsToCache);
+					return cache.addAll(['/',/worker.js','/main.js']);
 				})
 		);
 });
