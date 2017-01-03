ff6b9caea085d6cbd6b3f1ca7796fff6dd649ac1
Drakoun <davidboccara333@yahoo.fr>
2017-01-03 14:16:16 +0100

    init

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', function() {
-		navigator.serviceWorker.register('/Service_Worker/worker.js').then(function(registration) {
+		navigator.serviceWorker.register('/Service_Worker/serviceworker.js').then(function(registration) {
 			// Registration was successful
 			console.log('ServiceWorker registration successful with scope: ', registration.scope);
 	  
 			var CACHE_NAME = 'my-site-cache-v1';
 			var urlsToCache = ['/worker.js','/main.js'];
-
-			this..addEventListener('install', function(event) {
-				// Perform install steps
-					event.waitUntil(
-						caches.open(CACHE_NAME)
-							.then(function(cache) {
-								console.log('Opened cache');
-								return cache.addAll(urlsToCache);
-							})
-					);
-			});
 	  
 			var nombre = document.querySelector('#number');
 			var result = document.querySelector('.result');
